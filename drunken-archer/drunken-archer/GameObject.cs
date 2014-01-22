b4f6a3f9e511ba57a0f4f6e60676be9c8a3f140c
zeta0134 <thegamefreak0134@gmail.com>
2014-01-22 15:09:35 -0600

    Initial Commit
    A basic visual studio project, and the boilerplate code from the Lua2012
    project.

---
+++ drunken-archer/drunken-archer/GameObject.cs
@@ -0,0 +1,45 @@
+﻿using NLua;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DrunkenArcher
+{
+    class GameObject
+    {
+        static int next_id = 1;
+        int id;
+
+        public double x = 0;
+        public double y = 0;
+
+        struct acceleration
+        {
+            double x;
+            double y;
+        };
+
+        public GameObject(Lua vm)
+        {
+            id = next_id++;
+            bind_to_lua(vm);
+        }
+
+        public int ID()
+        {
+            return id;
+        }
+
+        public void bind_to_lua(Lua vm)
+        {
+
+            Console.WriteLine("Attempting to bind a lua thingy...");
+
+            string lua_name = "objects[" + id + "]";
+
+            //vm[lua_name] = this;
+            vm["object_to_bind"] = this;
+        }
+    }
+}
---
+++ drunken-archer/drunken-archer/lua/main.lua
@@ -0,0 +1,59 @@
+﻿print("Lua Started...")
+
+--setup some object related stuff
+
+function inherits(parent_class)
+	local new_class = {}
+	local new_mt = {__index=new_class}
+	local base = parent_class
+
+	if base then
+		setmetatable(new_class, {__index=base})
+	end
+	
+	function new_class.create(original)
+		local o = original or {}
+		if (base and base.create) then
+			--call the parent class's constructor
+			o = base.create(o)
+		end
+		setmetatable(o, new_mt)
+		return o
+	end
+
+	return new_class
+end
+
+Object = {}
+
+function Object.create(original)
+	print("Attempting to spawn an object...")
+	spawn_id = GameEngine.spawn() --returns the assigned object ID, which we need to keep track of
+
+	local o = original or {}
+	print("Spawned object ID:" .. spawn_id)
+	o.object = object_to_bind
+	objects[spawn_id] = o
+
+	return o
+end
+
+function Object:destroy()
+	print("Destroying an object...")
+	GameEngine.destroy(self)
+end
+
+--table for GameEngine stuff
+GameEngine = {}
+
+--global collection of all game objects
+objects = {}
+
+
+GameEngine.update = function()
+	for k, v in pairs(objects) do
+		if objects[k].update then
+			objects[k]:update()
+		end
+	end
+end
